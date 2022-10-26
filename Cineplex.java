ddff1df5ec0476054bd9d30857a1e04fb9790200
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2022-10-26 15:54:04 +0800

    reupdate

--- Cineplex.java
+++ Cineplex.java
@@ -3,7 +3,6 @@ public class Cineplex{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("bye");
+        //hello
     }
 }
-
-my mother salakau leader
