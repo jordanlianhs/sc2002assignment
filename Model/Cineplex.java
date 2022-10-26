9d4540c9e4810f69046a1cb678f7659c2460db98
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2022-10-26 16:56:03 +0800

    Updates w folder

---
+++ Model/Cineplex.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Cineplex{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("bye");
+        //hello
+    }
+}
