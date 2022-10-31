c4184bd91ba21df61d89907214ff00928745ec0f
AwhimAway <111488973+AwhimAway@users.noreply.github.com>
2022-10-31 16:51:50 +0800

    Login UI and Auth Method

--- UI/LoginUI.java
+++ UI/LoginUI.java
@@ -1,5 +1,30 @@
 package UI;
+import java.util.*;
 
 public class LoginUI {
-    
+	Scanner sc = new Scanner(System.in);
+	String usr, pass;
+	int token = 0;
+	public LoginUI() {}//Constructor
+    public int auth() {
+    	do {
+	    	System.out.println("***Login UI***");
+	    	System.out.println("Username: ");
+	    	usr = sc.nextLine();
+	    	System.out.println("Password: ");
+	    	pass = sc.nextLine();
+	    	
+	    	if (usr != "admin" || pass != "123321") {
+	    		System.out.println("Username or Password is incorrect!");
+	    		usr = null; //Resetting login credentials
+	    		pass = null;
+	    		token = 0;
+	    	}else {
+	    		System.out.println("Login as Administrator Successful");
+	    		token = 1;
+	    	}
+	    
+    	}while(token!=1);
+    	return token;
+    }
 }
