db4d4a565cf34ae3f1df570194a693975f6d0227
maviskhoo <115976411+maviskhoo@users.noreply.github.com>
2022-11-02 15:21:40 +0800

    Update MovieStatus.java

--- Model/MovieStatus.java
+++ Model/MovieStatus.java
@@ -1,18 +1,14 @@
 package Model;
 
-public enum MovieStatus {
-    COMING_SOON("Coming soon"),
-    PREVIEW("Preview"),
-    NOW_SHOWING("Now showing"),
-    END_OF_SHOWING("End of showing");
+public class moviestatus {
+    
+    public enum MovieStatus {
+        COMINGSOON {public String toString() {return "Coming Soon";}}, 
+        PREVIEW {public String toString() {return "Preview";}}, 
+        NOWSHOWING {public String toString() {return "Now Showing";}}, 
+        ENDOFSHOWING {public String toString() {return "End Of Showing";}}
+    } 
 
-    private final String status;
-
-    private MovieStatus(String status){
-        this.status = status;
-    }
-
-    public String getStatus(){
-        return status;
-    }
 }
+
+// System.out.println(MovieStatus.COMINGSOON.toString());
