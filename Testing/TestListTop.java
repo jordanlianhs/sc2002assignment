b0338bf5d412e2a5a80dab250d46523bc70f5eb6
Mark <markczy23@gmail.com>
2022-11-06 21:36:30 +0800

    Updated ListTop5UI
    ListTop5UI works. Added movie2.txt for testing

---
+++ Testing/TestListTop.java
@@ -0,0 +1,12 @@
+package Testing;
+import Model.*;
+import UI.ListTop5UI;
+
+public class TestListTop {
+
+    public static void main(String[] args){
+        ListTop5UI lister = new ListTop5UI();
+        lister.main();
+    }
+}
+   
