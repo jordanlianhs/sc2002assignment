f4e7f960d8da3067ba87a5b67c921143e43e942f
AwhimAway <111488973+AwhimAway@users.noreply.github.com>
2022-11-08 03:38:12 +0800

    Update HolidayUI.java

--- UI/HolidayUI.java
+++ UI/HolidayUI.java
@@ -310,20 +310,4 @@ public class HolidayUI {
 		}
 		return false;
 	}
-
-	// public static int getNumLines(File f) {
-
-	// int numLines = 0;
-	// try {
-	// Scanner s = new Scanner(f);
-	// while (s.hasNextLine()) {
-	// s.nextLine();
-	// numLines++;
-	// }
-	// s.close();
-	// } catch (FileNotFoundException e) {
-	// e.printStackTrace();
-	// }
-	// return numLines;
-	// }
 }
