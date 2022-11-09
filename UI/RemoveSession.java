c57d30480bdaa7e0e296c4417ff68d9c5541296d
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2022-11-09 15:55:14 +0800

    Delete Movie will remove all Sessions

--- UI/RemoveSession.java
+++ UI/RemoveSession.java
@@ -90,5 +90,81 @@ public class RemoveSession {
         }
         System.out.println("Session Deleted");
     }
+
+    public static void removeAllSession(String moviename) throws Exception{
+        Cineplex A = new Cineplex("Ang Mo Kio", "AMK");
+        Cineplex B = new Cineplex("Bukit Batok", "BBK");
+        Cineplex C = new Cineplex("Choa Chu Kang", "CCK");
+
+        System.out.println("Deleting all sessions in the Cineplexes");
+        String [] tempSession = {"AMK","BBK","CCK"};
+        for(int i =0; i<3;i++){
+            String cpCode;
+            cpCode = tempSession[i];
+            ArrayList<Session> temp;
+
+            if(cpCode.equals("AMK")){
+                temp =  A.getSessionList();
+            }
+            else if(cpCode.equals("BBK")){
+                temp =  B.getSessionList();
+            }
+            else{
+                temp =  C.getSessionList();
+            }
+
+            int size = temp.size();
+            int count = 0;
+            while(count <size){
+                if(temp.get(count).getMovie().getmovieName().equals(moviename)){
+                    temp.remove(count);
+                    size--;
+                    continue;
+                }
+                count++;
+            }
+
+
+            String fileName;
+            if(cpCode.equals("AMK")){
+                fileName= "Database/SessionRecordAMK.txt";
+            }
+            else if(cpCode.equals("BBK")){
+                fileName= "Database/SessionRecordBBK.txt";
+            }
+            else{
+                fileName= "Database/SessionRecordCCK.txt";
+            }
+            
+
+            PrintWriter writer = new PrintWriter(fileName);
+            writer.print("");
+            writer.close();
+            
+            size = temp.size();
+            count = 0;
+
+            if(cpCode.equals("AMK")){
+                while(count <size){
+                    temp.get(count).writeSession("AMK");
+                    System.out.println(size);
+                    count++;
+                }
+            }
+            else if(cpCode.equals("BBK")){
+                while(count <size){
+                    temp.get(count).writeSession("BBK");
+                    count++;
+                }
+            }
+            else{
+                while(count <size){
+                    temp.get(count).writeSession("CCK");
+                    count++;
+                }
+            }
+            System.out.println("Session Deleted");
+        }
+    }
     
 }
