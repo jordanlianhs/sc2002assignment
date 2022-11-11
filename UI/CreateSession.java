7a2a604b6f8bcec5ac5a729658da418dad101bca
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2022-11-11 17:31:47 +0800

    Javadocs for SessionCRUD

--- UI/CreateSession.java
+++ UI/CreateSession.java
@@ -9,6 +9,7 @@ import Model.*;
 public class CreateSession {
     static Scanner sc = new Scanner(System.in);
 
+    /**
     public static void main(String[] args) throws Exception {
 
         String temp;
