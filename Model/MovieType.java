3dbb3397dc12f38899776cf431ddb14ec3f7d714
AwhimAway <111488973+AwhimAway@users.noreply.github.com>
2022-11-11 21:39:56 +0800

    Javadocs

--- Model/MovieType.java
+++ Model/MovieType.java
@@ -1,13 +1,29 @@
 package Model;
 
+/**
+ * Types of movies
+ */
 public enum MovieType {
+
+    /**
+     * 2D movies, 3D Movies, 2D Blockbuster Movies, 3D BlockBuster movies.
+     */
     TWO_D("2D"), THREE_D("3D"), TWO_D_BB("2D_BLOCKBUSTER"), THREE_D_BB("3D_BLOCKBUSTER");
+
     private final String type;
 
-    private MovieType(String type){
+    /**
+     * This instance of movietype
+     */
+    private MovieType(String type) {
         this.type = type;
     }
 
+    /**
+     * Returns the type of movie as a String
+     * 
+     * @return movieType
+     */
     public String getType() {
         return this.type;
     }
