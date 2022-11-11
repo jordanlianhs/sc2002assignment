27acc636dfa1bc44971630d9624334474922b481
AwhimAway <111488973+AwhimAway@users.noreply.github.com>
2022-11-11 21:10:09 +0800

    Update lineCounter.java

--- UI/lineCounter.java
+++ UI/lineCounter.java
@@ -3,10 +3,23 @@ package UI;
 import java.io.*;
 import java.util.*;
 
+/**
+ * This class counts the number of lines in a txt file
+ */
 public class lineCounter {
+
+    /**
+     * Constructor for lineCounter class
+     */
     public lineCounter() {
     }
 
+    /**
+     * This method parses through every line in a txt file and increments a counter
+     * 
+     * @param f Input file to be scanned
+     * @return Number of lines in a file
+     */
     public int counter(File f) {
         int numLines = 0;
         try {
